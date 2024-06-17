193f20f316de606798f18207b710478efd0ce7b7
marceloyuzo <marceloyuzo0@hotmail.com>
2024-06-17 16:34:10 -0300

    removendo api local

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-   baseURL: "http://localhost:3000/"
-})
-
-export const apiCEP = axios.create({
-   baseURL: "https://viacep.com.br/ws/"
-})
